4302ab3e573e6add07eed3ed943fbba4997a2cb5
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-06-23 17:44:12 +0100

    feat: png images component

---
+++ assets/images/index.js
@@ -0,0 +1,4 @@
+export const Bagles = require("./Bagles.png");
+export const Cheese = require("./Cheese.png");
+export const Chicken = require("./Chicken.png");
+export const Saigon = require("./Saigon.png");
